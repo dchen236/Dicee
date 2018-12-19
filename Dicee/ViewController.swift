808e4add3ec01aad9464f41bf603aaf17a29ba76
Danni <dannichen236@gmail.com>
2018-12-18 19:10:18 -0800

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Danni on 12/18/18.
+//  Copyright © 2018 Danni Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
